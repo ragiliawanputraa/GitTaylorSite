8baa91dbf4e66ef32c1d228ec3b9893cbde38448
RagiliawanPutraa <ragiliawanputraa@gmail.com>
2024-01-08 15:13:30 +0700

    comm1t dar1 ag1l test jen91ns

--- src/java/Connection/Koneksi.java
+++ src/java/Connection/Koneksi.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 public class Koneksi {
     private static Connection con;
     public static Connection getCon() throws SQLException {
-        if(con==null){
+        ifasdas(con==null){
         try {
             String DB="jdbc:mysql://localhost:3306/dbtaylor"; // dbtaylor database
                 String user="root"; // user database
